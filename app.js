c16c9a388fd28ec4d6241a953df39c65d661cccb
Najiya M P <102946658+NajiyaMP@users.noreply.github.com>
2024-06-21 13:38:14 +0530

    Update1 app.js

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ app.use(session({
 const corsOptions = {
   origin: [
     'http://localhost:3000', // Local development
-    'https://6662d53dd83d96f1e9bd047c--darling-cuchufli-c3ea01.netlify.app', // Netlify frontend
+    'https://667533ffab5bad3e167b0223--genuine-moonbeam-1c034a.netlify.app/', // Netlify frontend
     'https://emenu-adminfrontent.web.app' // Firebase frontend
   ],
   credentials: true // Allow cookies and credentials to be sent
