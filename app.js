a4476f9f3d00a9398394731c99f08e45c6f837e5
Najiya M P <102946658+NajiyaMP@users.noreply.github.com>
2024-06-24 12:08:42 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// DEVELOPMENT
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -7,21 +8,76 @@ var connectDB =require('./config/db')
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var adminRouter = require('./routes/admin');
-const bodyParser = require('body-parser');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+const session = require('express-session');
 
 
-const cors = require('cors');
 
-const app = express();
+var app = express();
+connectDB();
 
-connectDB(); // Connect to MongoDB
 
-app.use(cors({
-  origin: '*'
+
+// // Middleware
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+// app.use(session({
+//   secret: 'mySecretKey',
+//   resave: false,
+//   saveUninitialized: true,
+//   cookie: { maxAge: 60000 }
+// }));
+
+app.use(express.json()); // For parsing application/json
+app.use(express.urlencoded({ extended: true })); // For parsing application/x-www-form-urlencoded
+
+app.use(session({
+  secret: 'your-secret-key',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { 
+    secure: true, // Set to true if using HTTPS
+    maxAge: 600000 // Session max age in milliseconds
+  }
 }));
 
+const corsOptions = {
+  origin: [
+    'http://localhost:3000', // Local development
+    'https://bucolic-liger-6c73bd.netlify.app', // Netlify frontend-client
+    'https://profound-pudding-3f97d2.netlify.app/'// Netlify admin-frontend
+   
+  ],
+  // credentials: true // Allow cookies and credentials to be sent
+};
+
+app.use(cors(corsOptions));
 
 
+// app.use(cors({
+//   origin: 'https://emenu-adminfrontent.web.app',
+//   credentials: true // Allow credentials to be sent
+// }));
+
+// For production (origin: '*')
+// app.use(cors({
+//   origin: '*',
+ 
+// }));
+
+
+
+//for developmen-hosting
+// app.use(cors({
+//   origin:'http://localhost:3000',
+//   credentials: true // Allow credentials to be sent
+// }));
+
+
+
+// view engine setup
 
 
 app.use(logger('dev'));
@@ -29,146 +85,28 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyParser.json());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/admin', adminRouter);
+
+
+// app.use('/images', express.static(path.join(__dirname, 'images')));
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+  // send error as JSON response
   res.status(err.status || 500);
-  res.render('error');
+  res.json({ error: err.message });
 });
 
 module.exports = app;
-
-
-
-
-
-
-
-
-// DEVELOPMENT
-// var createError = require('http-errors');
-// var express = require('express');
-// var path = require('path');
-// var cookieParser = require('cookie-parser');
-// var logger = require('morgan');
-// var connectDB =require('./config/db')
-// var indexRouter = require('./routes/index');
-// var usersRouter = require('./routes/users');
-// var adminRouter = require('./routes/admin');
-// var bodyParser = require('body-parser');
-// var cors = require('cors');
-// const session = require('express-session');
-
-
-
-// var app = express();
-// connectDB();
-
-
-
-// // // Middleware
-// // app.use(bodyParser.json());
-// // app.use(bodyParser.urlencoded({ extended: true }));
-
-// // app.use(session({
-// //   secret: 'mySecretKey',
-// //   resave: false,
-// //   saveUninitialized: true,
-// //   cookie: { maxAge: 60000 }
-// // }));
-
-// app.use(express.json()); // For parsing application/json
-// app.use(express.urlencoded({ extended: true })); // For parsing application/x-www-form-urlencoded
-
-// app.use(session({
-//   secret: 'your-secret-key',
-//   resave: false,
-//   saveUninitialized: true,
-//   cookie: { 
-//     secure: true, // Set to true if using HTTPS
-//     maxAge: 600000 // Session max age in milliseconds
-//   }
-// }));
-
-// const corsOptions = {
-//   origin: [
-//     'http://localhost:3000', // Local development
-//     'https://bucolic-liger-6c73bd.netlify.app', // Netlify frontend-client
-//     'https://profound-pudding-3f97d2.netlify.app/'// Netlify admin-frontend
-   
-//   ],
-//   // credentials: true // Allow cookies and credentials to be sent
-// };
-
-// app.use(cors(corsOptions));
-
-
-// // app.use(cors({
-// //   origin: 'https://emenu-adminfrontent.web.app',
-// //   credentials: true // Allow credentials to be sent
-// // }));
-
-// // For production (origin: '*')
-// // app.use(cors({
-// //   origin: '*',
- 
-// // }));
-
-
-
-// //for developmen-hosting
-// // app.use(cors({
-// //   origin:'http://localhost:3000',
-// //   credentials: true // Allow credentials to be sent
-// // }));
-
-
-
-// // view engine setup
-
-
-// app.use(logger('dev'));
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
-
-// app.use('/', indexRouter);
-// app.use('/users', usersRouter);
-// app.use('/admin', adminRouter);
-
-
-// // app.use('/images', express.static(path.join(__dirname, 'images')));
-
-
-// // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   next(createError(404));
-// });
-
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-//   // send error as JSON response
-//   res.status(err.status || 500);
-//   res.json({ error: err.message });
-// });
-
-// module.exports = app;
