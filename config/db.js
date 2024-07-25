6a9ec938b74db836560b86f3c436daf7596fda6f
Ashiquetkorambayil <102928710+Ashiquetkorambayil@users.noreply.github.com>
2024-07-25 12:34:25 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     // await mongoose.connect('mongodb+srv://ashiquetkorambayil:1W13EhIdOPaxYM1T@cluster0.ofyj5g5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
-      await mongoose.connect('mongodb+srv://mpnajiya118:bKdFkCIiKkxpjB2E@emenu.6fvc5f2.mongodb.net/?retryWrites=true&w=majority&appName=Emenu', {
+      await mongoose.connect('mongodb+srv://tecnaviswebsolutions:ZZVM6FawRrYnf4mt@emenu.pqhhkb3.mongodb.net/?retryWrites=true&w=majority&appName=Emenu', {
   
 
     });
